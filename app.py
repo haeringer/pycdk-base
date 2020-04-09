230307c78245fef50f7cc99906d4d4edb0f34175
Benjamin Häringer <benjamin.haeringer@europace.de>
2020-04-09 08:51:11 +0200

    Add tag

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
-
 from pycdk.pycdk_stack import PycdkStack
 
 
+env = core.Environment(region="eu-central-1")
+tags = {'bha':'teststack'}
+
 app = core.App()
-PycdkStack(app, "PycdkStack", env=core.Environment(region="eu-central-1"))
+PycdkStack(app, "PycdkStack", env=env, tags=tags)
 
 app.synth()
